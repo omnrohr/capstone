ca78364b48bf919050e1f4f840e3c350a5a84476
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-10-21 20:41:12 +0300

    Add auth configration to confit

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 import os
 
+# --------------------------------------------------------------------------------------#
+# AUTH0 config.
+# --------------------------------------------------------------------------------------#
+
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN', 'dev-uiw51rx8.us.auth0.com')
+ALGORITHMS = ['RS256']
+API_AUDIENCE = os.getenv('API_AUDIENCE', 'coffee')
+
 DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', 'admin')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'Opadah12')
